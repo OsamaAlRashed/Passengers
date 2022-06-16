f21c7cb02c51ec8418dbeac86ae02a6921c787d6
Osama Al-Rashed <Osama Al-Rashed@DESKTOP-8954161>
2022-06-16 16:06:15 +0300

    SignalR Access Token

--- Passengers.Order/RealTime/Hubs/OrderHub.cs
+++ Passengers.Order/RealTime/Hubs/OrderHub.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Passengers.Order.OrderService;
+using Passengers.SharedKernel.ExtensionMethods;
 using Passengers.SqlServer.DataBase;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using OrderSet = Passengers.Models.Order.Order;
@@ -26,16 +30,25 @@ namespace Passengers.Order.RealTime.Hubs
         private readonly PassengersDbContext dbContext;
         private readonly IUserConnectionManager _userConnectionManager;
         private readonly IOrderRepository orderRepository;
-        public OrderHub(PassengersDbContext dbContext, IUserConnectionManager userConnectionManager, IOrderRepository orderRepository)
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public OrderHub(PassengersDbContext dbContext, IUserConnectionManager userConnectionManager,
+            IOrderRepository orderRepository, IHttpContextAccessor httpContextAccessor)
         {
             this.dbContext = dbContext;
             _userConnectionManager = userConnectionManager;
             this.orderRepository = orderRepository;
+            this.httpContextAccessor = httpContextAccessor;
         }
         
         public override Task OnConnectedAsync()
         {
-            _userConnectionManager.KeepUserConnection(dbContext.CurrentUserId.Value, Context.ConnectionId);
+            var userId = dbContext.CurrentUserId;
+            if(userId == null)
+            {
+                userId = GetUserId();
+            }
+            _userConnectionManager.KeepUserConnection(userId.Value, Context.ConnectionId);
             return base.OnConnectedAsync();
         }
         
@@ -50,6 +63,17 @@ namespace Passengers.Order.RealTime.Hubs
         public async Task BroadcastMessage(string message)
         {
             await Clients.All.ReceiveMessage(message);
+        }
+
+        private Guid? GetUserId()
+        {
+            HttpContext? httpContext = httpContextAccessor.HttpContext;
+            var token = httpContextAccessor.HttpContext.Request.Query.Where(q => q.Key == "access_token").FirstOrDefault();
+            var stream = token.Value;
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = jsonToken as JwtSecurityToken;
+            return tokenS.Claims.First(claim => claim.Type == ClaimTypes.NameIdentifier).Value?.StringToGuid();
         }
     }
 } 
